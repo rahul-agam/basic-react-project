5cda1ca8e1e74bdf2620fce382670bd728a035c5
Rahul Agam <hidekiryuga0402@gmail.com>
2025-07-20 21:47:57 +0530

    corected hero component path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Navbar/Hero/Hero'
+import Hero from './Components/Hero/Hero'
 
 const App = () => {
   return (
