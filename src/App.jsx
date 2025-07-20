bd590800cc9903d6418275a4d66d69bcb9026403
Rahul Agam <hidekiryuga0402@gmail.com>
2025-07-20 22:24:58 +0530

    created program page & styled it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
-
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Title subTitle='OUR PROGRAM' title='What We Offer'/>
+        <Programs />
+      </div>
     </div>
   )
 }
